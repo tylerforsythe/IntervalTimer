8821dde6ddd851ba1205f49f0c0a2dd87e055404
Tyler Forsythe <forsythe.tyler@gmail.com>
2015-05-18 13:32:20 -0600

    Initial code commit where window has color and a hidden panel that, when clicked, will drag-move the window.

---
+++ IntervalTimer/IntervalTimerForm.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IntervalTimer
+{
+    public partial class IntervalTimerForm : Form
+    {
+        public IntervalTimerForm() {
+            InitializeComponent();
+
+            //Hidden panel over the entire thing to be draggable
+            panel1.MouseDown += panel1_MouseDown;
+        }
+
+
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HTCAPTION = 0x2;
+        [DllImport("User32.dll")]
+        public static extern bool ReleaseCapture();
+        [DllImport("User32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e) {
+            if (e.Button == MouseButtons.Left) {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HTCAPTION, 0);
+            }
+        }
+    }
+}
