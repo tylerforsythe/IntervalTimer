837510f2eb014efcefb152626f790c27fb5ece42
tylerforsythe <forsythe.tyler@gmail.com>
2015-05-23 20:53:14 -0600

    Timer display, pause, reset, and exit works. Rest mode transition does not yet. But the countdown does properly.

--- IntervalTimer/IntervalTimerForm.Designer.cs
+++ IntervalTimer/IntervalTimerForm.Designer.cs
@@ -26,14 +26,64 @@
         /// </summary>
         private void InitializeComponent() {
             this.panel1 = new System.Windows.Forms.Panel();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.btnTimerAction = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
+            this.panel1.Controls.Add(this.btnExit);
+            this.panel1.Controls.Add(this.btnCancel);
+            this.panel1.Controls.Add(this.lblTime);
+            this.panel1.Controls.Add(this.btnTimerAction);
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(283, 260);
             this.panel1.TabIndex = 0;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(85, 170);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 2;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // lblTime
+            // 
+            this.lblTime.AutoSize = true;
+            this.lblTime.Font = new System.Drawing.Font("Gill Sans Ultra Bold", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTime.ForeColor = System.Drawing.Color.White;
+            this.lblTime.Location = new System.Drawing.Point(86, 37);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(74, 30);
+            this.lblTime.TabIndex = 1;
+            this.lblTime.Text = "TIME";
+            // 
+            // btnTimerAction
+            // 
+            this.btnTimerAction.Location = new System.Drawing.Point(89, 86);
+            this.btnTimerAction.Name = "btnTimerAction";
+            this.btnTimerAction.Size = new System.Drawing.Size(75, 23);
+            this.btnTimerAction.TabIndex = 0;
+            this.btnTimerAction.Text = "Timer Start";
+            this.btnTimerAction.UseVisualStyleBackColor = true;
+            this.btnTimerAction.Click += new System.EventHandler(this.btnTimerAction_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(227, 226);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(45, 23);
+            this.btnExit.TabIndex = 3;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
             // 
             // IntervalTimerForm
             // 
@@ -45,6 +95,8 @@
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "IntervalTimerForm";
             this.Text = "Interval Timer";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -52,6 +104,10 @@
         #endregion
 
         private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnTimerAction;
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnExit;
     }
 }
 
--- IntervalTimer/IntervalTimerForm.cs
+++ IntervalTimer/IntervalTimerForm.cs
@@ -18,8 +18,19 @@ namespace IntervalTimer
 
             //Hidden panel over the entire thing to be draggable
             panel1.MouseDown += panel1_MouseDown;
+            btnCancel.Visible = false;
         }
 
+        private Timer _timer = null;
+        private TimeSpan _currentRemainingDuration;
+        private TimeSpan _oneSecondSpan = new TimeSpan(0, 0, 1);
+        private TimerState _state = TimerState.None; 
+
+        private enum TimerState {
+            None,
+            Work,
+            Rest
+        }
 
         public const int WM_NCLBUTTONDOWN = 0xA1;
         public const int HTCAPTION = 0x2;
@@ -33,6 +44,45 @@ namespace IntervalTimer
                 ReleaseCapture();
                 SendMessage(Handle, WM_NCLBUTTONDOWN, HTCAPTION, 0);
             }
+        }
+
+        private void btnTimerAction_Click(object sender, EventArgs e) {
+            if (_state == TimerState.None) {
+                _timer = new Timer();
+                _timer.Tick += TimerOnTick;
+                _timer.Interval = 1000;
+                _currentRemainingDuration = new TimeSpan(0, 0, 5);
+                UpdateTimerDisplay();
+                _state = TimerState.Work;
+                btnCancel.Visible = true;
+                _timer.Start();
+            }
+            else if (_state == TimerState.Work || _state == TimerState.Rest) {
+                _timer.Enabled = !_timer.Enabled;
+            }
+        }
+        
+        private void TimerOnTick(object sender, EventArgs eventArgs) {
+            _currentRemainingDuration = _currentRemainingDuration.Subtract(_oneSecondSpan);
+            UpdateTimerDisplay();
+            if (_currentRemainingDuration.TotalSeconds <= 0) {
+                _timer.Stop();
+                _state = TimerState.Rest;
+                //go into rest mode
+            }
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e) {
+            _timer.Stop();
+            lblTime.Text = "0";
+        }
+
+        private void UpdateTimerDisplay() {
+            lblTime.Text = _currentRemainingDuration.ToString("g");
+        }
+
+        private void btnExit_Click(object sender, EventArgs e) {
+            Application.Exit();
         }
     }
 }
